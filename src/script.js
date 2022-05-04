cc7f331578e43d499d46ea4b8f79f4e9dfa7c36c
Raymund Werling <raymund.werling@metadesign.com>
2022-05-04 22:27:47 +0200

    create canvas placeholders

--- src/script.js
+++ src/script.js
@@ -1,13 +1,10 @@
 import './style.css'
 import * as THREE from 'three'
-import { gsap } from 'gsap';
-import { ScrollTrigger } from "gsap/ScrollTrigger.js"
-gsap.registerPlugin(ScrollTrigger);
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-
-// assign canvas dom element to variable
+import { gsap } from 'gsap';
+gsap.registerPlugin(ScrollTrigger);
+import { ScrollTrigger } from "gsap/ScrollTrigger.js"
 
 
 // Scene
@@ -22,7 +19,7 @@ const textureLoader = new THREE.TextureLoader()
 const gltfLoader = new GLTFLoader()
 
 // Baked Texture
- const bakedTexture = textureLoader.load('baked.jpg');
+const bakedTexture = textureLoader.load('baked.jpg');
 bakedTexture.flipY = false;
 bakedTexture.encoding = THREE.sRGBEncoding;
 
@@ -51,27 +48,11 @@ gltfLoader.load(
     }
 )
 
-// const axesHelper = new THREE.AxesHelper( 5 );
-// scene.add( axesHelper );
-
-
-// set grid parameters and add grid to scene
-// const size = 15;
-// const divisions = 10;
-// const gridHelper = new THREE.GridHelper( size, divisions );
-// scene.add( gridHelper );
-
-
 //inital canvas sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight/1.5
 }
-
-// const sizes = {
-//     width: 1000,
-//     height: 600
-// }
 
 //resizer
 window.addEventListener('resize', () =>
@@ -106,8 +87,6 @@ document.getElementById('canvas1-container').appendChild( renderer.domElement );
 //document.getElementById('canvas2-container').appendChild( renderer.domElement );
 
 
-
-
 // scrollCamera moving on path
 const scrollCamera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height)
 scrollCamera.near = 1
@@ -115,18 +94,7 @@ scrollCamera.far = 30
 //scene.add(scrollCamera)
 
 
-// create path
-
-// const curve = new THREE.CatmullRomCurve3( [
-// 	new THREE.Vector3( -15, 10, 15 ),
-//     new THREE.Vector3( -10, 5, -10 ),
-// 	new THREE.Vector3( 2, 1, -10 ),
-// 	new THREE.Vector3( 2, 1, -4 ),
-// 	new THREE.Vector3( 2.5, 5, 13 )],
-//     false,
-// )
-
-
+// camera path
 const curve = new THREE.CatmullRomCurve3( [
 	new THREE.Vector3( 0, 25, 0 ),
     new THREE.Vector3( 0, 2, 12 )],
@@ -142,25 +110,8 @@ var scrollCameraTarget = new THREE.Vector3( 0, 1.5, -1 )
 var pathTarget = new THREE.Vector3(0,0,0)
 
 
-
-
-// create path2
-
-// const curve2 = new THREE.CatmullRomCurve3( [
-//     new THREE.Vector3(0, 1, -4),
-//     new THREE.Vector3(0, 1, 4)],
-//     false,
-// )
-
-// const points2 = curve2.getPoints(2);
-
-// Path Target2 object, scrollCamera will be attached to it later
-// var pathTarget2 = new THREE.Vector3(0,0,0)
-
-
-let cameraFov = 35;
-
 // camera
+let cameraFov = 35;
 const camera = new THREE.PerspectiveCamera(cameraFov, sizes.width / sizes.height);
 camera.position.set(20,10,20);
 
@@ -174,16 +125,6 @@ camera.position.set(20,10,20);
 // const scrollCameraHelper = new THREE.CameraHelper(scrollCamera);
 // scene.add(scrollCameraHelper);
 
-// // Orbit Controls
-// const controls = new OrbitControls( camera, renderer.domElement );
-
-
-// //assign button to varible
-// const exploreButton = document.getElementById("explore");
-
-// //add event listender to variable
-// exploreButton.addEventListener("click", null)
-
 
 
 // Scroll Animation
@@ -196,34 +137,15 @@ gsap.to(pathPercent, {
         start: "top 0%",
         end: "top 80%",
         scrub: 1,
-        markers: true,
+        markers: false,
         pin: true,
         toggleActions: "restart pause reverse reset"
     },
     value: 1 }
 );
 
-console.log(scrollCamera);
 
-
-// Animate Panel
-
-// gsap.to(".overlay" ,{
-//     scrollTrigger: {
-//         trigger: ".overlay",
-//         start: "top 0%",
-//         end: "bottom 0%",
-//         scrub: 1,
-//         markers: false,
-//         toggleActions: "restart pause reverse reset"
-//     },
-//     opacity: 0
-// });
-
-
-/**
- * Animate
- */
+// Animate
 const clock = new THREE.Clock()
 let previousTime = 0
 
@@ -232,9 +154,6 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
-    
-    //playScrollAnimations();
-    //curve.getPoint((clock.getElapsedTime() * speed) % 1.0, pathTarget)
 
     curve.getPoint(pathPercent.value, pathTarget); // update pathTarget position on path
 
