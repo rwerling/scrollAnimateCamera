93fb506d0a518392fac7332991556cb4e51bd528
Raymund Werling <raymund.werling@metadesign.com>
2022-05-23 16:38:58 +0200

    add orthographic camera

--- src/script.js
+++ src/script.js
@@ -167,11 +167,23 @@ gui.onChange(() =>
 
 
 // scrollCamera moving on path
-const scrollCamera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height)
-scrollCamera.near = 1
-scrollCamera.far = 30
+// const scrollCamera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height)
+// scrollCamera.near = 1
+// scrollCamera.far = 30
 //scene.add(scrollCamera)
 
+const aspectRatio = window.innerWidth / window.innerHeight;
+const cameraWidth = 150;
+const cameraHeight = cameraWidth / aspectRatio;
+
+const scrollCamera = new THREE.OrthographicCamera(
+    cameraWidth / -10, // left
+    cameraWidth / 10, // right
+    cameraHeight / 10, // top
+    cameraHeight / -10, // bottom
+    0, // near plane
+    1000 // far plane
+  );
 
 // camera dive path
 // const curve = new THREE.CatmullRomCurve3( [
